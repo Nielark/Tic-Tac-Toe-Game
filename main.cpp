675683ad02a4e3ae0388611ef9dd9b5523a061e8
Nielark <markdanielmagpantay2001@gmail.com>
2023-07-15 14:26:09 +0800

    Added the game logic in the gameLogic function

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 void displayBoard(char ArrBoard[3][3]);
 void player1Choice(char ArrBoard[3][3]);
 void player2Choice(char ArrBoard[3][3]);
+void gameLogic(char ArrBoard[3][3]);
 
 using namespace std;
-
+int p1Score = 0, p2Score = 0;
 int main()
 {
-    int p1Score = 0, p2Score = 0;
     char ArrBoard[3][3] ={{'1', '2', '3'},      // Array the will map the structure of the board
                           {'4', '5', '6'},
                           {'7', '8', '9'}};
@@ -19,9 +19,15 @@ int main()
     while(p1Score == 0 && p2Score == 0){
         player1Choice(ArrBoard);    // Function call for player 1's input
         displayBoard(ArrBoard);     // Displays the updated board
+        gameLogic(ArrBoard);
+
+        if(p1Score != 0 || p2Score != 0){
+            break;
+        }
 
         player2Choice(ArrBoard);    // Function call for player 2's input
         displayBoard(ArrBoard);     // Displays the updated board
+        gameLogic(ArrBoard);
     }
 
     return 0;
@@ -84,36 +90,69 @@ void player1Choice(char ArrBoard[3][3]){
 void player2Choice(char ArrBoard[3][3]){
     char p2;
 
+    top:
     // Ask input for player 2
     cout << "\n";
     cout << "Player 2, Enter your move: ";
     cin >> p2;
 
-    if(p2 == '1'){
+    if(p2 == '1' && ArrBoard[0][0] != 'X' && ArrBoard[0][0] != 'O'){
         ArrBoard[0][0] = 'O';
     }
-    else if(p2 == '2'){
+    else if(p2 == '2' && ArrBoard[0][1] != 'X' && ArrBoard[0][1] != 'O'){
         ArrBoard[0][1] = 'O';
     }
-    else if(p2 == '3'){
+    else if(p2 == '3' && ArrBoard[0][2] != 'X' && ArrBoard[0][2] != 'O'){
         ArrBoard[0][2] = 'O';
     }
-    else if(p2 == '4'){
+    else if(p2 == '4' && ArrBoard[1][0] != 'X' && ArrBoard[1][0] != 'O'){
         ArrBoard[1][0] = 'O';
     }
-    else if(p2 == '5'){
+    else if(p2 == '5' && ArrBoard[1][1] != 'X' && ArrBoard[1][1] != 'O'){
         ArrBoard[1][1] = 'O';
     }
-    else if(p2 == '6'){
+    else if(p2 == '6' && ArrBoard[1][2] != 'X' && ArrBoard[1][2] != 'O'){
         ArrBoard[1][2] = 'O';
     }
-    else if(p2 == '7'){
+    else if(p2 == '7' && ArrBoard[2][0] != 'X' && ArrBoard[2][0] != 'O'){
         ArrBoard[2][0] = 'O';
     }
-    else if(p2 == '8'){
+    else if(p2 == '8' && ArrBoard[2][1] != 'X' && ArrBoard[2][1] != 'O'){
         ArrBoard[2][1] = 'O';
     }
-    else if(p2 == '9'){
+    else if(p2 == '9' && ArrBoard[2][2] != 'X' && ArrBoard[2][2] != 'O'){
         ArrBoard[2][2] = 'O';
     }
+    else{
+        cout << "Invalid Input\n";
+        system("PAUSE");
+        displayBoard(ArrBoard);
+        goto top;
+    }
+}
+
+void gameLogic(char ArrBoard[3][3]){
+    if((ArrBoard[0][0] == 'X' && ArrBoard[0][1] == 'X' && ArrBoard[0][2] == 'X') ||
+       (ArrBoard[1][0] == 'X' && ArrBoard[1][1] == 'X' && ArrBoard[1][2] == 'X') ||
+       (ArrBoard[2][0] == 'X' && ArrBoard[2][1] == 'X' && ArrBoard[2][2] == 'X') ||
+       (ArrBoard[0][0] == 'X' && ArrBoard[1][0] == 'X' && ArrBoard[2][0] == 'X') ||
+       (ArrBoard[0][1] == 'X' && ArrBoard[1][1] == 'X' && ArrBoard[2][1] == 'X') ||
+       (ArrBoard[0][2] == 'X' && ArrBoard[1][2] == 'X' && ArrBoard[2][2] == 'X') ||
+       (ArrBoard[0][0] == 'X' && ArrBoard[1][1] == 'X' && ArrBoard[2][2] == 'X') ||
+       (ArrBoard[0][2] == 'X' && ArrBoard[1][1] == 'X' && ArrBoard[2][0] == 'X')
+       ){
+            p1Score++;
+        }
+
+    else if((ArrBoard[0][0] == 'O' && ArrBoard[0][1] == 'O' && ArrBoard[0][2] == 'O') ||
+       (ArrBoard[1][0] == 'O' && ArrBoard[1][1] == 'O' && ArrBoard[1][2] == 'O') ||
+       (ArrBoard[2][0] == 'O' && ArrBoard[2][1] == 'O' && ArrBoard[2][2] == 'O') ||
+       (ArrBoard[0][0] == 'O' && ArrBoard[1][0] == 'O' && ArrBoard[2][0] == 'O') ||
+       (ArrBoard[0][1] == 'O' && ArrBoard[1][1] == 'O' && ArrBoard[2][1] == 'O') ||
+       (ArrBoard[0][2] == 'O' && ArrBoard[1][2] == 'O' && ArrBoard[2][2] == 'O') ||
+       (ArrBoard[0][0] == 'O' && ArrBoard[1][1] == 'O' && ArrBoard[2][2] == 'O') ||
+       (ArrBoard[0][2] == 'O' && ArrBoard[1][1] == 'O' && ArrBoard[2][0] == 'O')
+       ){
+            p2Score++;
+        }
 }
